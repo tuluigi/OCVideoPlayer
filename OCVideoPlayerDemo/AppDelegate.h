4aba7eeae826172d36b050fabf359485b60ac2d9
tujinguo <tujinguo@corp.netease.com>
2015-07-16 18:01:55 +0800

    Initial Commit

---
+++ OCVideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCVideoPlayerDemo
+//
+//  Created by Luigi on 15/7/16.
+//  Copyright (c) 2015年 Luigi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
